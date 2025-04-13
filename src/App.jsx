793304191fc9899e989de65d36efa7205445fd40
Julian Pulecio <julian.pulecio1@gmail.com>
2025-04-13 17:13:55 -0500

    feature: srp login

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Login from './Components/Login';
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div>
+      <h1>React Cognito SRP Login</h1>
+      <Login />
+    </div>
+    </>
+  )
+}
+
+export default App
